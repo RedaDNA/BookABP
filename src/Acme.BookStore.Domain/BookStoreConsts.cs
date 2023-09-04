9514fdefd0ad4ec71fd73ad8d8ae363c4892cf0e
Reda Shehab <reda.123.rrr@gmail.com>
2023-09-04 14:53:56 +0300

    my commit

---
+++ src/Acme.BookStore.Domain/BookStoreConsts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Acme.BookStore;
+
+public static class BookStoreConsts
+{
+    public const string DbTablePrefix = "App";
+
+    public const string DbSchema = null;
+    
+}
